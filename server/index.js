fe6c0b99c62d763c884982c1870d43e23cf14e59
unknown <eper.david01@gmail.com>
2024-07-09 16:49:35 +0200

    fix port

--- server/index.js
+++ server/index.js
@@ -70,8 +70,8 @@ io.on('connection', (socket) => {
 })
 
 
-//const PORT = process.env.PORT || 3001;
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 server.listen(PORT, () => {
     console.log("Server is running");
 })
