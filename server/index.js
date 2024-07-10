93d60e38825d4bed6c01a1d5b263cb9657b96a60
unknown <eper.david01@gmail.com>
2024-07-10 13:42:24 +0200

    mobile scroll fixed

--- server/index.js
+++ server/index.js
@@ -13,7 +13,6 @@ let users = [];
 const io = new Server(server, {
     cors: {
         origin: ["https://letschatnow.vercel.app", "http://letschatnow.vercel.app"],
-        //origin: "http://localhost:3000",
         methods: ["GET", "POST"]
     }
 });
